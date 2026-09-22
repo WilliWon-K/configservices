557d0af695c83471e9675b87e56876c198829f72
WilliWon-K <williammourzelas@yahoo.com>
2026-09-22 14:29:04 +0200

    Delete vuln-engine directory

--- vuln-engine/collectors/__init__.py
+++
@@ -1,5 +0,0 @@
-from .nvd_delta import NVDDeltaCollector
-from .cisa_kev import CISAKEVCollector
-from .vendor_rss import VendorRSSCollector
-
-__all__ = ["NVDDeltaCollector", "CISAKEVCollector", "VendorRSSCollector"]
